7a41828de21b461d771a9d5e3ad30d9efc86ad0b
AgustinRow <agus.diaz.11@gmail.com>
2022-09-14 09:28:11 -0300

    feat: cron job executing task every 10 seconds

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Interval } from '@nestjs/schedule';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Interval(10000)
+  async handleCron() {
+    const date = this.appService.executeShell();
+    console.log('Date:', date);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { execFileSync } from 'child_process';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private readonly logger = new Logger(AppService.name);
+  executeShell(): string {
+    this.logger.debug('Executing file:');
+    const output = execFileSync('./src/date.sh', []);
+    return output.toString();
   }
 }
